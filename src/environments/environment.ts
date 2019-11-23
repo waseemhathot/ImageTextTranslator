7450386a7558913d721cd16a67a8551fbea30ebb
waseem <waseem.hathot@outlook.com>
2019-11-24 00:04:21 +0200

    changed api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   corsProxy: 'https://cors-anywhere.herokuapp.com/',
-  textDetectionApiKey: 'c16d140624cd49d4992c02d89f102469',
+  textDetectionApiKey: '727ce1eea0d441a285546c822d9e8ea5',
   textDetectionApiUrl: 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr',
   textTranslationApiKey: 'trnsl.1.1.20191103T204853Z.203b194a23a589e3.59e47c0f82f64fc33b27373ef13fa184a31e353a',
   textTranslationApiUrl: 'https://translate.yandex.net/api/v1.5/tr.json/translate'
